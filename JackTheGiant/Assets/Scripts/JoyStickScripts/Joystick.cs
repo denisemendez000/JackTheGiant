a2d9c51625b2cfd2944416e130c510f01ce69e16
Dmendz <denisemendez000@gmail.com>
2018-12-08 00:12:55 -0500

    Lesson 46 move with touch

---
+++ JackTheGiant/Assets/Scripts/JoyStickScripts/Joystick.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Joystick : MonoBehaviour , IPointerUpHandler, IPointerDownHandler{
+
+    private PlayerMoveJoystick playerMove;
+
+    void Start()
+    {
+        playerMove = GameObject.Find("Player").GetComponent<PlayerMoveJoystick>();
+    }
+
+    public void OnPointerUp(PointerEventData data)
+    {
+        if (gameObject.name == "Left")
+        {
+            Debug.Log("Touching the left");
+            playerMove.SetMoveLeft(true);
+        }
+        else
+        {
+            Debug.Log("Touching the left");
+            playerMove.SetMoveLeft(false);
+        }
+    }
+
+    public void OnPointerDown(PointerEventData data)
+    {
+        playerMove.StopMoving();
+    }
+
+}
---
+++ JackTheGiant/Assets/Scripts/PlayerScripts/PlayerMoveJoystick.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveJoystick : MonoBehaviour {
+
+    public float speed = 8f, maxVelocity = 4f;
+
+    private Rigidbody2D myBody;
+    private Animator anim;
+
+    private bool moveLeft, moveRight;
+
+    public void SetMoveLeft(bool move)
+    {
+        this.moveLeft = move;
+        this.moveRight = !move;
+    }
+
+    public void StopMoving()
+    {
+        this.moveLeft = this.moveRight = false;
+        anim.SetBool("Walk", false);
+    }
+
+    private void Awake()
+    {
+        // First function that is executed
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
+
+	void FixedUpdate () {
+
+        if (moveLeft)
+        {
+            MoveLeft();
+        }
+
+        if (moveRight)
+        {
+            MoveRight();
+        }
+
+    }
+
+    void MoveLeft()
+    {
+        float forceX = 0f;
+        float vel = Mathf.Abs(myBody.velocity.x);
+        Vector2 temp = transform.localScale;
+        if (vel < maxVelocity)
+        {
+            forceX = -speed;
+        }
+        anim.SetBool("Walk", true);
+        temp.x = -1.3f;
+        transform.localScale = temp;
+        myBody.AddForce(new Vector2(forceX, 0));
+    }
+
+    void MoveRight()
+    {
+        float forceX = 0f;
+        float vel = Mathf.Abs(myBody.velocity.x);
+        Vector2 temp = transform.localScale;
+        if (vel < maxVelocity)
+        {
+            forceX = speed;
+        }
+        anim.SetBool("Walk", true);
+        temp.x = 1.3f;
+        transform.localScale = temp;
+        myBody.AddForce(new Vector2(forceX, 0));
+    }
+}//
+
